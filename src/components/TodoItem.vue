7eb8770dd8e5c7977a618431ea048294564d826c
enjirtmnv <shkensinio@gmail.com>
2021-01-31 21:15:37 +0300

    use computed

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,13 +3,16 @@
         <span v-bind:class="{done: item.completed}">
             <input type="checkbox" v-on:change="item.completed = !item.completed">
             <strong>{{index + 1}}</strong>
-                    {{item.title}}
+                    {{item.title | uppercase}}
         </span>
         <button class="rm" v-on:click="$emit('remove-item', item.id)">&times;</button>
     </li>
 </template>
 
 <script>
+
+    // filters - трансформируют данные внутри шаблона
+
     export default {
         props: {
             item: {
@@ -17,6 +20,12 @@
                 required: true
             },
             index: Number,
+        },
+        filters: {
+            uppercase(value) {
+                console.log(value);
+                return value.toUpperCase()
+            }
         }
     }
 </script>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -4,9 +4,15 @@
         <AddItem
                 @add-item="addItem"
         />
+        <select v-model="filter">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="not-completed">Not Completed</option>
+        </select>
+        <Loader v-if="loading === true"/>
         <TodoList
-                v-if="todos.length"
-                v-bind:todos="todos"
+                v-else-if="filteredItems.length"
+                v-bind:todos="filteredItems"
                 @remove-item="removeItem"
         />
         <p v-else>No items!</p>
@@ -17,35 +23,63 @@
 <script>
     import TodoList from '@/components/TodoList'
     import AddItem from '@/components/AddItem'
+    import Loader from '@/components/Loader'
+
     export default {
         name: 'App',
         data() {
-            return{
+            return {
                 todos: [
                     // {id: 1, title: 'gucci gang', completed: false},
                     // {id: 2, title: 'prada re-edition 2005', completed: false},
                     // {id: 3, title: 'off-white', completed: false},
-                ]
+                ],
+                loading: true,
+                filter: 'all'
             }
         },
         mounted() {
             fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
                 .then(response => response.json())
-                .then(json =>
-                    this.todos = json
+                .then(json => {
+                        setTimeout(() => {
+                            this.todos = json;
+                            this.loading = false
+                        }, 1000)
+                    }
                 )
+        },
+        // watch: {
+        //     filter(value){
+        //         console.log(value)
+        //     }
+        // },
+        computed: {
+            filteredItems() {
+                if (this.filter === 'all' ){
+                    return this.todos
+                }
+
+                if (this.filter === 'completed' ){
+                    return this.todos.filter(t => t.completed)
+                }
+
+                if (this.filter === 'completed' ){
+                    return this.todos.filter(t => !t.completed)
+                }
+            }
         },
         methods: {
             removeItem(id) {
                 this.todos = this.todos.filter(i => id !== i.id)
             },
-            addItem(newItem){
+            addItem(newItem) {
                 this.todos.push(newItem);
                 console.log(this.todos)
             }
         },
         components: {
-            TodoList, AddItem
+            TodoList, AddItem, Loader
         }
     }
 </script>
